4b3d826f40426a746ce0afa14e08dc331e09a9b2
wynn645 <wynnus645@gmail.com>
2024-04-28 02:09:37 +0200

    .

--- backend/src/main/java/com/umb/tradingapp/controller/RootController.java
+++ backend/src/main/java/com/umb/tradingapp/controller/RootController.java
@@ -1,15 +1,11 @@
 package com.umb.tradingapp.controller;
 
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
-
 import com.umb.tradingapp.security.controller.HttpServletRequest;
 
 import java.io.IOException;
